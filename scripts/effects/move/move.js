c9b5441cd830bd4bdb40c439cd5488215f8dbe69
Adriano <wp@petrucci.ch>
2018-07-18 14:10:43 +0200

    arduino code
    effect bug when generating arduino code

--- scripts/effects/move/move.js
+++ scripts/effects/move/move.js
@@ -98,18 +98,30 @@ class EffectMove extends Effect
     var code = super.GetArduinoCode(stripid, effectname, leds);
     
     code += "  if(millis() - " + s + ".effStart < " + this.delay + " * (" + s + ".effStep)) return 0x00;\n";
-    code += "  uint8_t e,r,g,b;\n";
-    code += "    e = (" + s + ".effStep * " + this.delay + ") / " + this.options['duration'] + ";\n";
-    code += "    r = " + this.colors[1].red + " * ( e ) + " + this.colors[0].red + " * ( 1.0 - e );\n";
-    code += "    g = " + this.colors[1].green + " * ( e ) + " + this.colors[0].green + " * ( 1.0 - e );\n";
-    code += "    b = " + this.colors[1].blue + " * ( e ) + " + this.colors[0].blue + " * ( 1.0 - e );\n";
-    code += "    for(uint16_t j=0;j<" + leds + ";j++) {\n";
-    code += "      if((j % " + this.options['every'] + ") == 0)\n";
-    code += "        " + s + ".strip.setPixelColor(j, r, g, b);\n";
-    code += "      else\n";
-    code += "        " + s + ".strip.setPixelColor(j, " + this.colors[0].red + ", " + this.colors[0].green + ", " + this.colors[0].blue + ");\n";
-    code += "    }\n";
-    code += "  }\n";
+    if(this.options['rotate'])
+    {
+      if(this.options['toLeft'])
+        code += "  uint32_t c = " + s + ".strip.getPixelColor(0);\n";
+      else
+        code += "  uint32_t c = " + s + ".strip.getPixelColor(" + (leds-1) + ");\n";
+    }
+    if(this.options['toLeft'])
+    {
+      code += "  for(uint16_t j=0;j<" + leds + "-1;j++) \n";
+      code += "    " + s + ".strip.setPixelColor(j, " + s + ".strip.getPixelColor(j+1)); \n";
+    }
+    else
+    {
+      code += "  for(uint16_t j=" + leds + "-1;j>0;j--) \n";
+      code += "    " + s + ".strip.setPixelColor(j, " + s + ".strip.getPixelColor(j-1)); \n";
+    }
+    if(this.options['rotate'])
+    {
+      if(this.options['toLeft'])
+        code += "    " + s + ".strip.setPixelColor(" + leds + "-1, c); \n";
+      else
+        code += "    " + s + ".strip.setPixelColor(0, c); \n";
+    }
     code += "  if(" + s + ".effStep >= " + (this.steps) + ") {" + s + ".Reset(); return 0x03; }\n";
     code += "  else " + s + ".effStep++;\n";
     code += "  return 0x01;\n";
